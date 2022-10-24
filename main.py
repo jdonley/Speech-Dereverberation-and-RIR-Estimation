8c28fc9f68666736b477cba7d1ecbb28c4c30a3b
Jacob Donley <jrd089@uowmail.edu.au>
2022-10-24 12:45:16 -0700

    Add model test

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ test_loader  = DareDataloader("test")
 
 # PyTorch Lightning Train
 trainer = pl.Trainer(limit_train_batches=100, max_epochs=1)
-trainer.fit(model=autoencoder, train_dataloaders=train_loader)
+trainer.fit(model=autoencoder, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
+# ===========================================================
+# PyTorch Lightning Test
+trainer.test(model=autoencoder, dataloaders=test_loader, ckpt_path="best")
 
 # ===========================================================
 checkpoint = "./lightning_logs/version_0/checkpoints/epoch=0-step=100.ckpt"
