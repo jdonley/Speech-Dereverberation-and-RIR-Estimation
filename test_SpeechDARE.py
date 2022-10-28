ee3ab7aca43cf3404b8ac997acf6d79ace5f9ff2
jdonley <jdonley@fb.com>
2022-10-27 20:41:34 -0700

    Update test_SpeechDARE.py

--- test_SpeechDARE.py
+++ test_SpeechDARE.py
@@ -1,7 +1,7 @@
 from models.lightning_model import LitAutoEncoder
-from datasets.reverb_speech_data import DareDataloader
-import pytorch_lightning as pl
-from utils import getTestConfig
+#from datasets.reverb_speech_data import DareDataloader
+#import pytorch_lightning as pl
+#from utils import getTestConfig
 
 def dummy_flow():
     # ===========================================================
@@ -9,35 +9,35 @@ def dummy_flow():
     autoencoder = LitAutoEncoder()
 
     # Data Loaders
-    train_loader = DareDataloader("train")
-    val_loader   = DareDataloader("val")
-    test_loader  = DareDataloader("test")
+    #train_loader = DareDataloader("train")
+    #val_loader   = DareDataloader("val")
+    #test_loader  = DareDataloader("test")
 
     # PyTorch Lightning Train
-    trainer = pl.Trainer(
-        limit_train_batches = getTestConfig()['train_batches'],
-        limit_val_batches   = getTestConfig()['val_batches'],
-        limit_test_batches  = getTestConfig()['test_batches'],
-        max_epochs          = getTestConfig()['max_epochs'],
-        log_every_n_steps   = getTestConfig()['log_every_n_steps'],
-        accelerator         = getTestConfig()['accelerator'],
-        devices             = getTestConfig()['devices'],
-        strategy            = getTestConfig()['strategy']
-        )
+    #trainer = pl.Trainer(
+    #    limit_train_batches = getTestConfig()['train_batches'],
+    #    limit_val_batches   = getTestConfig()['val_batches'],
+    #    limit_test_batches  = getTestConfig()['test_batches'],
+    #    max_epochs          = getTestConfig()['max_epochs'],
+    #    log_every_n_steps   = getTestConfig()['log_every_n_steps'],
+    #    accelerator         = getTestConfig()['accelerator'],
+    #    devices             = getTestConfig()['devices'],
+    #    strategy            = getTestConfig()['strategy']
+    #    )
 
-    trainer.fit(
-        model=autoencoder,
-        train_dataloaders=train_loader,
-        val_dataloaders=val_loader
-        )
+    #trainer.fit(
+    #    model=autoencoder,
+    #    train_dataloaders=train_loader,
+    #    val_dataloaders=val_loader
+    #    )
 
     # ===========================================================
     # PyTorch Lightning Test
-    trainer.test(
-        model=autoencoder,
-        dataloaders=test_loader,
-        ckpt_path="best"
-        )
+    #trainer.test(
+    #    model=autoencoder,
+    #    dataloaders=test_loader,
+    #    ckpt_path="best"
+    #    )
     
     return True
 
