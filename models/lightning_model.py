644b861b15e6453f2031cf897ce832c0e44e56ba
Jacob Donley <jrd089@uowmail.edu.au>
2022-10-28 00:53:15 -0700

    Fixed sizes in UNet architecture using output_padding=1

--- models/lightning_model.py
+++ models/lightning_model.py
@@ -69,14 +69,14 @@ class ErnstUnet(pl.LightningModule):
         self.conv7   = nn.Sequential(nn.Conv2d(512, 512, 5, stride=2, padding=2), nn.BatchNorm2d(512), nn.LeakyReLU(0.2))
         self.conv8   = nn.Sequential(nn.Conv2d(512, 512, 5, stride=2, padding=2), nn.BatchNorm2d(512), nn.ReLU())
         
-        self.deconv1 = nn.Sequential(nn.ConvTranspose2d( 512, 1024, 5, stride=2, padding=2), nn.BatchNorm2d(1024), nn.Dropout2d(p=0.5), nn.ReLU())
-        self.deconv2 = nn.Sequential(nn.ConvTranspose2d(1024, 1024, 5, stride=2, padding=2), nn.BatchNorm2d(1024), nn.Dropout2d(p=0.5), nn.ReLU())
-        self.deconv3 = nn.Sequential(nn.ConvTranspose2d(1024, 1024, 5, stride=2, padding=2), nn.BatchNorm2d(1024), nn.Dropout2d(p=0.5), nn.ReLU())
-        self.deconv4 = nn.Sequential(nn.ConvTranspose2d(1024, 1024, 5, stride=2, padding=2), nn.BatchNorm2d(1024), nn.ReLU())
-        self.deconv5 = nn.Sequential(nn.ConvTranspose2d(1024,  512, 5, stride=2, padding=2), nn.BatchNorm2d(512),  nn.ReLU())
-        self.deconv6 = nn.Sequential(nn.ConvTranspose2d( 512,  256, 5, stride=2, padding=2), nn.BatchNorm2d(256),  nn.ReLU())
-        self.deconv7 = nn.Sequential(nn.ConvTranspose2d( 256,  128, 5, stride=2, padding=2), nn.BatchNorm2d(128),  nn.ReLU())
-        self.deconv8 = nn.Sequential(nn.ConvTranspose2d( 128,    1, 5, stride=2, padding=2), nn.Tanh())
+        self.deconv1 = nn.Sequential(nn.ConvTranspose2d( 512, 512, 5, stride=2, padding=2, output_padding=1), nn.BatchNorm2d(512), nn.Dropout2d(p=0.5), nn.ReLU())
+        self.deconv2 = nn.Sequential(nn.ConvTranspose2d(1024, 512, 5, stride=2, padding=2, output_padding=1), nn.BatchNorm2d(512), nn.Dropout2d(p=0.5), nn.ReLU())
+        self.deconv3 = nn.Sequential(nn.ConvTranspose2d(1024, 512, 5, stride=2, padding=2, output_padding=1), nn.BatchNorm2d(512), nn.Dropout2d(p=0.5), nn.ReLU())
+        self.deconv4 = nn.Sequential(nn.ConvTranspose2d(1024, 512, 5, stride=2, padding=2, output_padding=1), nn.BatchNorm2d(512), nn.ReLU())
+        self.deconv5 = nn.Sequential(nn.ConvTranspose2d(1024, 256, 5, stride=2, padding=2, output_padding=1), nn.BatchNorm2d(256),  nn.ReLU())
+        self.deconv6 = nn.Sequential(nn.ConvTranspose2d( 512, 128, 5, stride=2, padding=2, output_padding=1), nn.BatchNorm2d(128),  nn.ReLU())
+        self.deconv7 = nn.Sequential(nn.ConvTranspose2d( 256,  64, 5, stride=2, padding=2, output_padding=1), nn.BatchNorm2d(64),  nn.ReLU())
+        self.deconv8 = nn.Sequential(nn.ConvTranspose2d( 128,   1, 5, stride=2, padding=2, output_padding=1), nn.Tanh())
     
     def training_step(self, batch, batch_idx):
         # training_step defines the train loop.
@@ -94,13 +94,13 @@ class ErnstUnet(pl.LightningModule):
         c8Out = self.conv8(c7Out) # (1 x 1 x 512)
 
         d1Out = self.deconv1(c8Out) # (2 x 2 x 1024)
-        d2Out = self.deconv2(t.cat([d1Out, c7Out], axis=-1)) # (4 x 4 x 1024)
-        d3Out = self.deconv3(t.cat([d2Out, c6Out], axis=-1)) # (8 x 8 x 1024)
-        d4Out = self.deconv4(t.cat([d3Out, c5Out], axis=-1)) # (16 x 16 x 1024)
-        d5Out = self.deconv5(t.cat([d4Out, c4Out], axis=-1)) # (32 x 32 x 512)
-        d6Out = self.deconv6(t.cat([d5Out, c3Out], axis=-1)) # (64 x 64 x 256)
-        d7Out = self.deconv7(t.cat([d6Out, c2Out], axis=-1)) # (128 x 128 x 128)
-        d8Out = self.deconv8(t.cat([d7Out, c1Out], axis=-1)) # (256 x 256 x 1)
+        d2Out = self.deconv2(t.cat((d1Out, c7Out), dim=1)) # (4 x 4 x 1024)
+        d3Out = self.deconv3(t.cat((d2Out, c6Out), dim=1)) # (8 x 8 x 1024)
+        d4Out = self.deconv4(t.cat((d3Out, c5Out), dim=1)) # (16 x 16 x 1024)
+        d5Out = self.deconv5(t.cat((d4Out, c4Out), dim=1)) # (32 x 32 x 512)
+        d6Out = self.deconv6(t.cat((d5Out, c3Out), dim=1)) # (64 x 64 x 256)
+        d7Out = self.deconv7(t.cat((d6Out, c2Out), dim=1)) # (128 x 128 x 128)
+        d8Out = self.deconv8(t.cat((d7Out, c1Out), dim=1)) # (256 x 256 x 1)
 
         loss   = nn.functional.mse_loss(d8Out, y)
         # Logging to TensorBoard by default
@@ -123,13 +123,13 @@ class ErnstUnet(pl.LightningModule):
         c8Out = self.conv8(c7Out) # (1 x 1 x 512)
 
         d1Out = self.deconv1(c8Out) # (2 x 2 x 1024)
-        d2Out = self.deconv2(t.cat([d1Out, c7Out], axis=-1)) # (4 x 4 x 1024)
-        d3Out = self.deconv3(t.cat([d2Out, c6Out], axis=-1)) # (8 x 8 x 1024)
-        d4Out = self.deconv4(t.cat([d3Out, c5Out], axis=-1)) # (16 x 16 x 1024)
-        d5Out = self.deconv5(t.cat([d4Out, c4Out], axis=-1)) # (32 x 32 x 512)
-        d6Out = self.deconv6(t.cat([d5Out, c3Out], axis=-1)) # (64 x 64 x 256)
-        d7Out = self.deconv7(t.cat([d6Out, c2Out], axis=-1)) # (128 x 128 x 128)
-        d8Out = self.deconv8(t.cat([d7Out, c1Out], axis=-1)) # (256 x 256 x 1)
+        d2Out = self.deconv2(t.cat((d1Out, c7Out), dim=1)) # (4 x 4 x 1024)
+        d3Out = self.deconv3(t.cat((d2Out, c6Out), dim=1)) # (8 x 8 x 1024)
+        d4Out = self.deconv4(t.cat((d3Out, c5Out), dim=1)) # (16 x 16 x 1024)
+        d5Out = self.deconv5(t.cat((d4Out, c4Out), dim=1)) # (32 x 32 x 512)
+        d6Out = self.deconv6(t.cat((d5Out, c3Out), dim=1)) # (64 x 64 x 256)
+        d7Out = self.deconv7(t.cat((d6Out, c2Out), dim=1)) # (128 x 128 x 128)
+        d8Out = self.deconv8(t.cat((d7Out, c1Out), dim=1)) # (256 x 256 x 1)
 
         loss   = nn.functional.mse_loss(d8Out, y)
         # Logging to TensorBoard by default
@@ -152,13 +152,13 @@ class ErnstUnet(pl.LightningModule):
         c8Out = self.conv8(c7Out) # (1 x 1 x 512)
 
         d1Out = self.deconv1(c8Out) # (2 x 2 x 1024)
-        d2Out = self.deconv2(t.cat([d1Out, c7Out], axis=-1)) # (4 x 4 x 1024)
-        d3Out = self.deconv3(t.cat([d2Out, c6Out], axis=-1)) # (8 x 8 x 1024)
-        d4Out = self.deconv4(t.cat([d3Out, c5Out], axis=-1)) # (16 x 16 x 1024)
-        d5Out = self.deconv5(t.cat([d4Out, c4Out], axis=-1)) # (32 x 32 x 512)
-        d6Out = self.deconv6(t.cat([d5Out, c3Out], axis=-1)) # (64 x 64 x 256)
-        d7Out = self.deconv7(t.cat([d6Out, c2Out], axis=-1)) # (128 x 128 x 128)
-        d8Out = self.deconv8(t.cat([d7Out, c1Out], axis=-1)) # (256 x 256 x 1)
+        d2Out = self.deconv2(t.cat((d1Out, c7Out), dim=1)) # (4 x 4 x 1024)
+        d3Out = self.deconv3(t.cat((d2Out, c6Out), dim=1)) # (8 x 8 x 1024)
+        d4Out = self.deconv4(t.cat((d3Out, c5Out), dim=1)) # (16 x 16 x 1024)
+        d5Out = self.deconv5(t.cat((d4Out, c4Out), dim=1)) # (32 x 32 x 512)
+        d6Out = self.deconv6(t.cat((d5Out, c3Out), dim=1)) # (64 x 64 x 256)
+        d7Out = self.deconv7(t.cat((d6Out, c2Out), dim=1)) # (128 x 128 x 128)
+        d8Out = self.deconv8(t.cat((d7Out, c1Out), dim=1)) # (256 x 256 x 1)
 
         loss   = nn.functional.mse_loss(d8Out, y)
         # Logging to TensorBoard by default
