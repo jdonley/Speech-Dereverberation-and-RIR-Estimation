865b5d32f48a12c30cf57d7378a27d4e25dffc22
Jacob Donley <jrd089@uowmail.edu.au>
2022-11-03 01:53:19 -0700

    Update test conditions

--- test_SpeechDARE.py
+++ test_SpeechDARE.py
@@ -2,9 +2,10 @@ from models.lightning_model import *
 from datasets.reverb_speech_data import DareDataModule
 from pytorch_lightning.callbacks import ModelCheckpoint
 import pytorch_lightning as pl
-from utils import getConfig
+from utils import getTestConfig
 
 def dummy_flow():
+    cfg = getTestConfig()
     # ===========================================================
     # PyTorch Lightning Models
     model = LitAutoEncoder()
@@ -17,20 +18,20 @@ def dummy_flow():
     # Checkpoints
     checkpoint_callback = ModelCheckpoint(
         monitor="val_loss",
-        dirpath=getConfig()['checkpoint_dirpath'],
+        dirpath=cfg['checkpoint_dirpath'],
         filename=model.name+"-{epoch:02d}-{val_loss:.2f}",
     )
 
     # PyTorch Lightning Train
     trainer = pl.Trainer(
-        limit_train_batches    = getConfig()['train_batches'],
-        limit_val_batches      = getConfig()['val_batches'],
-        limit_test_batches     = getConfig()['test_batches'],
-        max_epochs             = getConfig()['max_epochs'],
-        log_every_n_steps      = getConfig()['log_every_n_steps'],
-        accelerator            = getConfig()['accelerator'],
-        devices                = getConfig()['devices'],
-        strategy               = getConfig()['strategy'],
+        limit_train_batches    = cfg['train_batches'],
+        limit_val_batches      = cfg['val_batches'],
+        limit_test_batches     = cfg['test_batches'],
+        max_epochs             = cfg['max_epochs'],
+        log_every_n_steps      = cfg['log_every_n_steps'],
+        accelerator            = cfg['accelerator'],
+        devices                = cfg['devices'],
+        strategy               = cfg['strategy'],
         callbacks=[checkpoint_callback]
         )
 
