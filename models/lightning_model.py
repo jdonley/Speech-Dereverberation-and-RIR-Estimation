c103c422844184207f6147a2b1232eea82fa6ae6
Jacob Donley <jacob.donley10@gmail.com>
2022-11-22 02:30:25 -0800

    Add new losses and some minor tweaks

--- models/lightning_model.py
+++ models/lightning_model.py
@@ -358,6 +358,7 @@ class SpeechDAREUnet_v2(pl.LightningModule):
         self.nhop = nhop
         self.nfrms = nfrms
         self.win = t.hann_window(self.nfft)
+        self.eps = 1e-16
         self.init()
 
     def init(self):
@@ -444,8 +445,13 @@ class SpeechDAREUnet_v2(pl.LightningModule):
         y_hat_c = y_predict[:,0,:,:] + 1j*y_predict[:,1,:,:]
         
         y_hat_c_t = t.fft.irfft(y_hat_c,n=yt.shape[1],dim=1).squeeze()
-        mse_time = nn.functional.mse_loss(y_hat_c_t, yt) * 1e5
-        err_time = nn.functional.l1_loss(y_hat_c_t, yt) * 1e3
+        mse_time = nn.functional.mse_loss(y_hat_c_t, yt)
+        err_time = nn.functional.l1_loss(y_hat_c_t, yt)
+        y_hat_c_t_abs_log = (y_hat_c_t.abs()+self.eps).log()
+        yt_abs_log = (yt.abs()+self.eps).log()
+        mse_time_abs_log = nn.functional.mse_loss(y_hat_c_t_abs_log, yt_abs_log)
+        err_time_abs_log = nn.functional.l1_loss(y_hat_c_t_abs_log, yt_abs_log)
+        kld_time_abs_log = nn.functional.kl_div(y_hat_c_t_abs_log,yt_abs_log,log_target=True).abs()
         
         mse_real = nn.functional.mse_loss(t.real(y_hat_c),t.real(y_c))
         mse_imag = nn.functional.mse_loss(t.imag(y_hat_c),t.imag(y_c))
@@ -462,28 +468,38 @@ class SpeechDAREUnet_v2(pl.LightningModule):
         # err_phase = t.sum(t.abs(y_c)/t.sum(t.abs(y_c)) * t.abs(t.angle(y_c)-t.angle(y_hat_c)))
         mse_phase = nn.functional.mse_loss(y1,y_hat1) + nn.functional.mse_loss(y2,y_hat2)
         err_phase = nn.functional.l1_loss(y1,y_hat1) + nn.functional.l1_loss(y2,y_hat2)
+        y_a = t.tensor(np.unwrap(t.angle(y_c).cpu().detach().numpy(),axis=1)).to(self.device)
+        y_hat_a = t.tensor(np.unwrap(t.angle(y_hat_c).cpu().detach().numpy(),axis=1)).to(self.device)
+        mse_phase_un = nn.functional.mse_loss(y_a,y_hat_a)
+        err_phase_un = nn.functional.l1_loss(y_a,y_hat_a)
 
         #loss = err_real + err_imag + 2*err_abs
-        loss_mse = mse_abs + mse_phase + mse_time
-        loss_err = err_abs + err_phase + err_time
+        loss_err = err_abs + err_phase + err_phase_un * 1e-4
+        loss_mse = mse_abs + mse_phase + mse_phase_un * 1e-4
 
         self.log(type+"_loss_err", loss_err )
         self.log(type+"_loss_mse", loss_mse )
         self.log(type+"_err_phase",err_phase)
+        self.log(type+"_err_phase_un",err_phase_un)
         self.log(type+"_err_abs",  err_abs  )
         self.log(type+"_err_real", err_real )
         self.log(type+"_err_imag", err_imag )
         self.log(type+"_err_time", err_time )
+        self.log(type+"_err_time_abs_log", err_time_abs_log )
         self.log(type+"_mse_phase",mse_phase)
+        self.log(type+"_mse_phase_un",mse_phase_un)
         self.log(type+"_mse_abs",  mse_abs  )
         self.log(type+"_mse_real", mse_real )
         self.log(type+"_mse_imag", mse_imag )
         self.log(type+"_mse_time", mse_time )
+        self.log(type+"_mse_time_abs_log", mse_time_abs_log )
+        self.log(type+"_kld_time_abs_log", kld_time_abs_log )
 
         return \
             loss_err, loss_mse, \
-            err_real, err_imag, err_abs, err_phase, err_time, \
-            mse_real, mse_imag, mse_abs, mse_phase, mse_time, \
+            err_real, err_imag, err_abs, err_phase, err_phase_un, err_time, err_time_abs_log, \
+            mse_real, mse_imag, mse_abs, mse_phase, mse_phase_un, mse_time, mse_time_abs_log, \
+            kld_time_abs_log, \
             y_hat_c
 
     def configure_optimizers(self):
@@ -498,27 +514,38 @@ class SpeechDAREUnet_v2(pl.LightningModule):
             a = x.cpu().detach().numpy()
             b = y.cpu().detach().numpy()
             c = y_hat.cpu().detach().numpy()
+            
+            y_c = y[0,0,:,:].float() + 1j*y[0,1,:,:].float()
+            x_a = np.unwrap(np.angle((a[0,0,:,:] + 1j*a[0,1,:,:])),axis=0)
+            y_a = np.unwrap(np.angle(y_c.cpu().detach().numpy()),axis=0)
+            y_hat_a = np.unwrap(np.angle(y_hat_c.cpu().detach().numpy()),axis=1)
             ax1.imshow(a[0,0,:,:], aspect='auto')
             ax2.imshow(b[0,0,:,:], aspect='auto')
             ax3.imshow(c[0,0,:,:], aspect='auto')
-            ax4.imshow(a[0,1,:,:], aspect='auto')
-            ax5.imshow(b[0,1,:,:], aspect='auto')
-            ax6.imshow(c[0,1,:,:], aspect='auto')
+            ax4.plot(x_a[:,0], linewidth=0.1)
+            ax5.plot(y_a[:,0], linewidth=0.1)
+            ax6.plot(y_hat_a[0,:,0], linewidth=0.1)
             # ax4.imshow(np.angle(a[0,0,:,:] + 1j*a[0,1,:,:]), aspect='auto')
             # ax5.imshow(np.angle(b[0,0,:,:] + 1j*b[0,1,:,:]), aspect='auto')
             # ax6.imshow(np.angle(c[0,0,:,:] + 1j*c[0,1,:,:]), aspect='auto')
-            ax7.imshow(np.log(np.abs(a[0,0,:,:] + 1j*a[0,1,:,:])), aspect='auto')
-            ax8.imshow(np.log(np.abs(b[0,0,:,:] + 1j*b[0,1,:,:])), aspect='auto')
-            ax9.imshow(np.log(np.abs(c[0,0,:,:] + 1j*c[0,1,:,:])), aspect='auto')
+            ax7.plot(np.mean(np.log(np.abs(a[0,0,:,:] + 1j*a[0,1,:,:])),axis=1), linewidth=0.1)
+            ax8.plot(np.log(np.abs(b[0,0,:,0] + 1j*b[0,1,:,0])), linewidth=0.1)
+            ax9.plot(np.log(np.abs(c[0,0,:,0] + 1j*c[0,1,:,0])), linewidth=0.1)
             rir_est = np.fft.irfft(y_hat_c[0,:,:].cpu().squeeze().detach().numpy())
-            gs = fig.add_gridspec(4,2)
+            rir = np.fft.irfft(y_c.cpu().squeeze().detach().numpy())
+            gs = fig.add_gridspec(4,3)
             ax10 = fig.add_subplot(gs[3, 0])
             ax10.plot(rir_est[:rir_est.shape[0]//16], linewidth=0.1)
+            ax10.plot(rir[:rir_est.shape[0]//16], linewidth=0.1)
             ax11 = fig.add_subplot(gs[3, 1])
             ax11.plot(rir_est, linewidth=0.1)
+            ax11.plot(rir, linewidth=0.1)
+            ax12 = fig.add_subplot(gs[3, 2])
+            ax12.plot(np.log(np.abs(rir_est)), linewidth=0.1)
+            ax12.plot(np.log(np.abs(rir)), linewidth=0.1)
             plt.savefig("./images/2_"+str(self.current_epoch)+".png",dpi=1200)
             tb = self.logger.experiment
-            tb.add_figure('Fig1', plt.gcf())
+            tb.add_figure('Fig1', plt.gcf(), global_step=self.global_step)
             plt.close()
         return 0
     
